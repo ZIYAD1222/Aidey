3baa02a5c632a3ac11ba114a5f01e7763e83a9ea
Ziyad alsulaiman <131574632+ZIYAD1222@users.noreply.github.com>
2026-07-28 16:38:01 -0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,1 @@
+import { defineConfig } from 'vite' import react from '@vitejs/plugin-react' export default defineConfig({ plugins: [react()], preview: { allowedHosts: ['aidey-frontend-production.up.railway.app'], }, })
